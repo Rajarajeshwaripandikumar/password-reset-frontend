81a13bcdc0d8af2ffe429b721b7d059d9996f7dd
Rajarajeshwari <rajarajeshwaripandikumar@gmail.com>
2025-10-29 11:55:40 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,11 +1,9 @@
-import 'bootstrap/dist/css/bootstrap.min.css';
-import React from "react";
-import ReactDOM from "react-dom/client";
-import App from "./App";
-import "bootstrap/dist/css/bootstrap.min.css";
+// src/index.js
+import 'bootstrap/dist/css/bootstrap.min.css'; // only import once
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
+import './index.css'; // Tailwind / global CSS - import BEFORE rendering
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
+const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(<App />);
-
-import './index.css';
-
